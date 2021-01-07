8557e0e4726798f17bb7a0b761e57084abda1bfe
346776813 <346776813@qq.com>
2021-01-07 21:45:59 +0800

    人脸识别1.1

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "2C7LY4wLLmUmRR9DybJgb4VcTMwndXUxpd3t9SA1bGfg";
+    public static final String SDK_KEY = "7TCHTyfHY4i9rZNTVeygz75qDnc8rHL5jAaS6r2KfJeq";
+}
