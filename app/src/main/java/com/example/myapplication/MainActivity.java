51e9b1512d0ae4111933c581de77a0135abb554b
346776813 <346776813@qq.com>
2021-01-07 10:50:02 +0800

    第一次更新

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,33 +5,70 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mBtnLogin;
     private Button mBtnRegister;
-    private EditText mTUserName;
-    private EditText mTPassword;
+    private EditText mEtUserName;
+    private EditText mEtPassword;
+    private TextView mTvForget;
+    private TextView mTvFace;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         mBtnLogin = findViewById(R.id.btn_1);
         mBtnRegister = findViewById(R.id.btn_2);
-        mTUserName = findViewById(R.id.et_1);
-        mTPassword = findViewById(R.id.et_2);
-        mBtnLogin.setOnClickListener((View.OnClickListener) this);
-        mBtnRegister.setOnClickListener((View.OnClickListener) this);
-    }
-
-    public void onClick(View v) {
-        //获取输入的信息
-        String username = mTUserName.getText().toString();
-        String password = mTPassword.getText().toString();
-        String ok = "登陆成功";
-        String fail = "密码错误，登陆失败";
-        Intent intent = null;
+        mEtUserName = findViewById(R.id.et_1);
+        mEtPassword = findViewById(R.id.et_2);
+        mTvForget = findViewById(R.id.tv_1);
+        mTvFace = findViewById(R.id.tv_2);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=mEtUserName.getText().toString();
+                String password=mEtPassword.getText().toString();
+                if(true){               //登录成功
+                    Toast.makeText(getApplicationContext(),"登录成功",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"登录失败",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=mEtUserName.getText().toString();
+                String password=mEtPassword.getText().toString();
+                if(true){               //注册成功
+                    Toast.makeText(getApplicationContext(),"注册成功",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"注册失败",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        mTvForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,RetrieveActivity.class);
+                startActivity(intent);
+            }
+        });
+        mTvFace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,FaceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
